ac245c59e6eb19f7a776a20debc3ad7fe16ea645
Rishuraj3732 <103851611+Rishuraj3732@users.noreply.github.com>
2025-08-31 18:03:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const counterElement=document.getElementById("counter");
+const incrementBtn=document.getElementById("incrementBtn");
+incrementBtn.addEventListener("click",function() {
+	let currvalue=parseInt(counterElement.textContent);
+	alert("Current value before increment: " + currentValue);
+
+      // Increment the value and update paragraph
+      counterElement.textContent = currentValue + 1;
+	
+})
